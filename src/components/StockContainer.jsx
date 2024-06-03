2ec8b75cda37537f9b1f68c12f0c2eb52857cc32
Philip Clapper <maestro7@gmail.com>
2024-06-03 13:41:38 -0400

    Add SearchBar, StockContainer components

---
+++ src/components/StockContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Stock from './Stock';
+
+function StockContainer() {
+    // Map over the array of stocks and create a Stock component for each stock
+    const stockList = stocks.map((stock) => <Stock key={stock.id} stock={stock} />);
+
+    // Render the list of stocks and a heading
+    return (
+        <div>
+            <h2>Stocks</h2>
+            {stockList}
+        </div>
+    );
+}
+
+export default StockContainer;
