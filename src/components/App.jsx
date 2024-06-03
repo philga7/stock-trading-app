b2d644b85a3fb7ab5473c6eaca419873661fbb0c
Philip Clapper <maestro7@gmail.com>
2024-06-03 17:06:22 -0400

    Add filtering and sorting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,8 +9,17 @@ function App() {
   // Define state variables using the useState hook
   const [stocks, setStocks] = useState([]);
   const [portfolio, setPortfolio] = useState([]);
+  const [sortBy, setSortBy] = useState('Alphabetically');
+  const [filterBy, setFilterBy] = useState('Tech');
 
   // Fetch the list of stocks from the API using the useEffect hook
+  /*
+  useEffect(() => {
+    fetch('http://localhost:3001/stocks')
+      .then((response) => response.json())
+      .then(setStocks);
+  }, []);
+  */
 
   // Handler function for when the user adds a stock to their portfolio
   function handleAddStock(stockToAdd) {
@@ -29,20 +38,40 @@ function App() {
     })
   }
 
+  // Sort the list of stocks based on the current sort option
+  const sortedStocks = [...stocks].sort((stock1, stock2) => {
+    if (sortBy === 'Alphabetically') {
+      return stock1.name.localeCompare(stock2.name);
+    } else {
+      return stock2.price - stock1.price;
+    }
+  });
+
+  // Filter the list of stocks based on the current filter option
+  const filteredStocks = sortedStocks.filter(
+    (stock) => stock.type === filterBy
+  );
+
   return (
     <div>
       {/* Render the SearchBar component */}
-      <SearchBar />
+      <SearchBar 
+        sortBy={sortBy}
+        onChangeSort={setSortBy}
+        filterBy={filterBy}
+        onChangeFilter={setFilterBy}
+      />
       {/* Render the Header component */}
-      <header className="App-header">
+      <header>
         <Header />
       </header>
       {/* Render the StockContainer and PortfolioContainer */}
       <div className='row'>
         <div className='col-8'>
           <StockContainer
-            stocks={stocks}
-            onStockClick={handleAddStock}
+            stocks={filteredStocks}
+            onAddStock={handleAddStock}
+            onRemoveStock={handleRemoveStock}
           />
         </div>
       </div>
